17d605d07866976524cf4c2bf30e0a8154fa6741
Yun Chuan Ngin <yunchuann@gmail.com>
2025-03-24 00:04:13 +0800

    Revert to train-test split (instead of K-fold)

--- salary.py
+++ salary.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator
-from sklearn.model_selection import KFold, cross_validate, train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.preprocessing import OrdinalEncoder
 
@@ -12,38 +11,54 @@ _df_jobs_clean = pd.read_csv('data/jobs_clean_nt.csv')
 df_X = _df_jobs_clean.drop(columns=["normalized_salary_log10", "normalized_salary", "min_salary", "max_salary", "med_salary"])
 df_y = _df_jobs_clean["normalized_salary"]
 
+df_X_train, df_X_test, df_y_train, df_y_test = train_test_split(df_X, df_y, test_size=0.2, random_state=42)
 
 # --------------------------------------
 # Functions for train and test datasets
 # --------------------------------------
-def train_evaluate_model(model: BaseEstimator):
-    cv = KFold(n_splits=5, shuffle=True, random_state=42)
-    scores = cross_validate(
-        model, df_X, df_y, cv=cv, 
-        return_train_score=True,
-        return_estimator=True,
-        scoring=["r2", "neg_mean_squared_error", "neg_mean_absolute_error"]
-    )
-    
-    train_r2 = scores["train_r2"].mean()
-    test_r2 = scores["test_r2"].mean()
-    print(f"Mean CV train R2: {train_r2:.4f}")
-    print(f"Mean CV test R2: {test_r2:.4f}")
 
-    train_rmse = np.sqrt(-scores["train_neg_mean_squared_error"].mean())
-    test_rmse = np.sqrt(-scores["test_neg_mean_squared_error"].mean())
-    print(f"Mean CV train RMSE: {train_rmse:.4f}")
-    print(f"Mean CV test RMSE: {test_rmse:.4f}")
+def get_train_dataset():
+    return df_X_train, df_y_train
 
-    train_mae = -scores["train_neg_mean_absolute_error"].mean()
-    test_mae = -scores["test_neg_mean_absolute_error"].mean()
-    print(f"Mean CV train MAE: {train_mae:.4f}")
-    print(f"Mean CV test MAE: {test_mae:.4f}")
+def get_test_dataset():
+    return df_X_test, df_y_test
+
+def evaluate_train_predictions(y_train_pred):
+    '''
+    Evaluate the train predictions against the true train values (normalized_salary)
+    '''
+    y_train = df_y_train.values
+
+    train_r2 = r2_score(y_train, y_train_pred)
+    print(f"Train R2: {train_r2:.4f}")
+
+    train_rmse = np.sqrt(mean_squared_error(y_train, y_train_pred))
+    print(f"Train RMSE: {train_rmse:.4f}")
+
+    train_mae = mean_absolute_error(y_train, y_train_pred)
+    print(f"Train MAE: {train_mae:.4f}")
+
+def evaluate_test_predictions(y_test_pred):
+    '''
+    Evaluate the test predictions against the true test values (normalized_salary)
+    '''
+    y_train_mean = df_y_train.mean()
+    y_test = df_y_test.values
+
+    test_r2 = r2_score(y_test, y_test_pred)
+    print(f"Test R2: {test_r2:.4f}")
+
+    test_rmse = np.sqrt(mean_squared_error(y_test, y_test_pred))
+    print(f"Test RMSE: {test_rmse:.4f}")
+
+    test_mae = mean_absolute_error(y_test, y_test_pred)
+    print(f"Test MAE: {test_mae:.4f}")
+
+    test_mae_mean = mean_absolute_error(y_test, [y_train_mean]*len(y_test))
+    improvement = ((test_mae_mean-test_mae) / test_mae_mean)*100
     print(f'On average, our predicted salaries are ${test_mae:.2f} off the true salaries')
+    print(f'This is {improvement:.2f}% better than a naive global mean')
 
-    # Return the best model based on test R2
-    best_estimator = scores['estimator'][np.argmax(scores['test_r2'])]
-    return best_estimator
 
 
 ## --------------------------------------
